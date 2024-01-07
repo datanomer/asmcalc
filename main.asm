692d67f25e6112c7614b4eaa34185a5df6e3ed79
datanomer <niccsacc@protonmail.com>
2024-01-07 21:40:29 +0200

    dosnt wok ihasama

--- main.asm
+++ main.asm
@@ -1,38 +1,61 @@
 ; Simple calculator made in assembly by nx86
 ; TODO: addition, multiplication, etc. other calculation and algebra shit 
 
-section .data
-    message: db  "calculator\n", 0h
-    frstnum: db "1st number: ", 0h
-    action: db "action(*, /, +, -): ", 0h
-    sndnum: db "2nd number: ", 0h
-    resultm: db "result: ", 0h
-    msglen: equ $-message
-section .bss
+SECTION .data
+    message: db  "calculator\n", 0Ah
+    frstnum: db "1st number: ", 0Ah
+    action: db "action(*, /, +, -): ", 0Ah
+    sndnum: db "2nd number: ", 0Ah
+    resultm: db "result: ", 0Ah
+
+SECTION .bss
 
 input1: resb 10 ; reserve 10 bytes for first number
 input2: resb 5 ; reserve 5 bytes for the action
 input3: resb 10 ; reserve 10 bytes for the second number
     
 
-section .text
+SECTION .text
 
-    global main
+global main
+
+    slen:
+        push rbx
+        mov rbx, rax
+    
+
+
+    printmsg:
+
+        call    slen
+ 
+        mov     rdx, rax
+        pop     rax
+ 
+        mov     rcx, rax
+        mov     rbx, 1
+        mov     rax, 4
+        int     80h
+ 
+        pop     rbx
+        pop     rcx
+        pop     rdx
+    
 
     main:
-        mov rax, 1
-        mov rdi, 1
-        mov rsi, message
-        mov rdx, msglen
-        syscall
-
+        mov rax, message
+        jmp printmsg
 
         mov rbx, 0
         mov rax, 1
         int 80h
 
-    question:
 
-                
+;    addition:
+
+
+ ;   substraction:
+
+
 
 
