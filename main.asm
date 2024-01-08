b3f984aa38c83cc8bf3faae31e2b23bc2b613b6c
datanomer <niccsacc@protonmail.com>
2024-01-08 22:55:15 +0200

    tryna do daily commits n shit, code dont work

--- main.asm
+++ main.asm
@@ -2,7 +2,7 @@
 ; TODO: addition, multiplication, etc. other calculation and algebra shit 
 
 SECTION .data
-    message: db  "calculator\n", 0Ah
+    message: db  "calculator", 0Ah
     frstnum: db "1st number: ", 0Ah
     action: db "action(*, /, +, -): ", 0Ah
     sndnum: db "2nd number: ", 0Ah
@@ -18,15 +18,42 @@ input3: resb 10 ; reserve 10 bytes for the second number
 SECTION .text
 
 global main
+    
+    main:
+        mov rax, message
+        call printmsg
+
+        mov rbx, 0
+        mov rax, 1
+        int 80h
 
     slen:
+        
         push rbx
         mov rbx, rax
+        call nextchar
+
+    nextchar:
     
-
-
+        cmp     byte[rax], 0
+        jz      finished
+        inc     rax
+        jmp     nextchar
+    
+    finished:
+        sub     rax, rbx
+        mov     rdx, rax
+        mov     rcx, message
+        mov     rbx, 1
+        mov     rax, 4
+        int     80h
+ 
     printmsg:
 
+        push    rdx
+        push    rcx
+        push    rbx
+        push    rax
         call    slen
  
         mov     rdx, rax
@@ -42,19 +69,11 @@ global main
         pop     rdx
     
 
-    main:
-        mov rax, message
-        jmp printmsg
-
-        mov rbx, 0
-        mov rax, 1
-        int 80h
+   
+;  Todo:  addition:
 
 
-;    addition:
-
-
- ;   substraction:
+ ;   TODO: subtraction:
 
 
 
