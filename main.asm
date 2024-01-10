14179855a89585565fabe8ca9089f6a079bcd849
datanomer <niccsacc@protonmail.com>
2024-01-10 23:00:40 +0200

    stuff2

--- main.asm
+++ main.asm
@@ -4,11 +4,18 @@
 extern print_Achar
 extern os_return
 
+SECTION .data
+    msg: db  "calculator", 0Ah, 0h
+    frstnum: db "1st number: ", 0Ah, 0h
+    action: db "action(*, /, +, -): ", 0Ah, 0h
+    sndnum: db "2nd number: ", 0Ah, 0h
+    resultm: db "result: ", 0Ah, 0h
+
 SECTION .bss
 
-input1: resb 10 ; reserve 10 bytes for first number
-input2: resb 5 ; reserve 5 bytes for the action
-input3: resb 10 ; reserve 10 bytes for the second number
+input1: resb 255 ; reserve 255 bytes for first number
+input2: resb 255 ; reserve 255 bytes for the action
+input3: resb 255 ; reserve 255 bytes for the second number
     
 
 SECTION .text
@@ -16,13 +23,21 @@ SECTION .text
 global main
     
     main:
-        mov rax, message
+        mov rax, msg
         call printmsg
 
+        mov rdx, 255
+        mov rcx, input1
         mov rbx, 0
-        mov rax, 1
+        mov rax, 3
         int 80h
-  
+
+        mov rdx, 255
+        mov rcx, input2
+        mov rbx, 0
+        mov rax, 3
+    
+        call exit 
 ;  Todo:  addition:
 
 
