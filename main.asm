65b7c34a176935f80432c85bce78252a85dbeacb
datanomer <niccsacc@protonmail.com>
2024-01-11 23:57:33 +0200

    At least no segfault

--- main.asm
+++ main.asm
@@ -3,19 +3,17 @@
 %include "funcs.asm"
 extern print_Achar
 extern os_return
+extern printf
 
 SECTION .data
-    msg: db  "calculator", 0Ah, 0h
-    frstnum: db "1st number: ", 0Ah, 0h
-    action: db "action(*, /, +, -): ", 0Ah, 0h
-    sndnum: db "2nd number: ", 0Ah, 0h
-    resultm: db "result: ", 0Ah, 0h
+    msg: db  "Calculator\0", 0
+    fmt: db  "%s", 10, 0
 
 SECTION .bss
 
-input1: resb 255 ; reserve 255 bytes for first number
-input2: resb 255 ; reserve 255 bytes for the action
-input3: resb 255 ; reserve 255 bytes for the second number
+input1: resb 4 ; reserve 4 bytes for first number
+input2: resb 4 ; reserve 4 bytes for the action
+input3: resb 4 ; reserve 4 bytes for the second number
     
 
 SECTION .text
@@ -26,20 +24,26 @@ global main
         mov rax, msg
         call printmsg
 
-        mov rdx, 255
+        mov rdx, 4
         mov rcx, input1
         mov rbx, 0
         mov rax, 3
-        int 80h
-
-        mov rdx, 255
+        
+        mov rdx, 4
         mov rcx, input2
         mov rbx, 0
         mov rax, 3
-    
-        call exit 
-;  Todo:  addition:
 
+;        call add
+
+        call exit
+
+        
+
+
+;  Todo:  addition:
+;    add:
+         
 
  ;   TODO: subtraction:
 
