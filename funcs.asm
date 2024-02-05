d5e7c3de72818a56aafeaab76b2d0ba49e776501
datanomer <niccsacc@protonmail.com>
2024-02-05 23:55:13 +0200

    removed some functions

--- funcs.asm
+++ funcs.asm
@@ -1,23 +1,6 @@
 extern printf
 SECTION .text
 
-    add:
-        push r8
-        push r9
-        add r8 ,r9
-        pop r9
-        push r8
-        mov rbx, r8
-        push rbx
-    
-    sub: 
-        push r8
-        push r9
-        sub r8, r9
-        pop r9
-        push r8
-        mov rbx, r8
-        push rbx
 
     exit:
         mov rbx, 0
