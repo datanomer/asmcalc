6406932c986bfece73bb53f9c51a9b4c70b658b5
datanomer <niccsacc@protonmail.com>
2024-02-12 23:24:57 +0200

    ei vittu toimi mikään vielkää aaaa

--- main.asm
+++ main.asm
@@ -1,5 +1,5 @@
 ; Simple calculator made in assembly by nx86
-; TODO: addition, multiplication, etc. other calculation and algebra shit 
+; TODO: convert input to printable integer, basic algebra operations and checking input for invalid stuff 
 %include "funcs.asm"
 extern printf
 extern print_Achar
@@ -8,14 +8,17 @@ extern os_return
 
 SECTION .data
        msg db "Assembly calculator",20 , 0
-       msg2 db "ADD or SUB?", 10, 0
-       fmt db "%s", 10, 0 
-
+       msg2 db "ADDITION: ", 11, 0
+       fmts db "%s", 10, 0
+       fmtx db "%x", 10, 0
+       ;ints db "0123456789", 10, 0
 SECTION .bss
 
-input1: resb 20 ; reserve 20 bytes for first number
-input2: resb 20 ; reserve 20 bytes for second number
-result: resb 20 ; result
+input1len: equ 12 ; reserve 20 bytes for first number
+input1: resb input1len
+input2len: equ 12 ; reserve 20 bytes for second number
+input2: resb input2len
+result: rest 100
 
 SECTION .text
 
@@ -27,66 +30,43 @@ _start:
     ; message printing        
     push rbp
     mov rsi, msg
-    mov rdi, fmt
-    mov rax, 1
-    call printf
-    
-    pop rbp
-    push rbp
-    mov rsi, msg2
-    mov rdi, fmt
+    mov rdi, fmts
     mov rax, 1
     call printf
     int 80h
 
     ; take 2 inputs
-    mov rdx, 20
-    mov rcx, input1
-    mov rbx, 0
-    mov rax, 3
-    push rcx
-    int 80h
-
-    mov rdx, 20
-    mov rcx, input2
-    mov rbx, 0
-    mov rax, 3
-
-    int 80h
-        
-    ; call add routine located in: funcs.asm
+    ;sys_read 1 input
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, input1
+    mov rdx, input1len
+    syscall
+    ; sys_read for second input
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, input2
+    mov rdx, input2len
+    syscall
+    
+    ; call add routine
     call add
-    ;convert rbx register value to ascii
-    call intoas  
+    ;convert input value to ascii-coded decimal and print it to screen
+    call atoi
 
-    add:
-        mov rax, [input1]
-        sub rax, 48
-        mov rbx, [input2]
-        sub rbx, 48
-        add rax, rbx
-        add rax, 48
-        mov [result], rax
-    
-    sub: 
-        push r8
-        push r9
-        sub r8, r9
-        pop r9
-        push r8
-        mov r10, r8
-        push r10
-    
-    intoas:
-        push r10
-        mov r10, [result]
-        mov rax, 4
-        mov rbx, 1
-        mov rcx, r10
-        mov rdx, 1
-        int 80h
-
-
+    mov rsi, rbx
+    mov rdi, fmts
+    mov rax, 1
+    call printf
+    int 80h
+    ;exit 
     call exit
 
+    ; add inputs together
+    add:
+        mov rax, [input1]
+        mov rbx, [input2] 
+        add rax, rbx
+        mov rcx, rax
+        push rcx
 
